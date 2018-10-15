2447ae6215be4c5d6e1c0de8399fcac8cb3e1405
uoon lee <uoon.dev@jobis.co>
2018-10-15 10:53:15 +0900

    add custom error component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -69,12 +70,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            return <ErrorBoundary key={person.id}>
+              <Person 
+                click={() => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            </ErrorBoundary>
           })}
         </div>
       )
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import classes from './Person.modules.css';
+import classes from './Person.module.css';
 
 const person = (props) => {
+  const rnd = Math.random();
+  if (rnd > 0.7) {
+    throw new Error('Something went wrong');
+  }
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
